8697a0b0498238172451d463559b594166da1b9e
SamuelSoriano80 <samuelsoriano80@gmail.com>
2025-05-31 12:37:33 -0600

    dont tell me that was it

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const productData = new ProductData();
-const productList = new ProductList(productData);
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
+
+productList.init();
